6237e346fe734e39180269a6f0f7f0574a13c3fd
Dominique Nix <raquel_0216@yahoo.com>
2023-02-16 20:33:07 -0600

    changed login and sign up to only need username and password

--- public/js/login.js
+++ public/js/login.js
@@ -3,13 +3,13 @@ const loginBtn = document.getElementById('submit');
 
 loginBtn.addEventListener('click', async (e) => {
     e.preventDefault();
-    const email = document.getElementById('email').value.trim()
+    const name = document.getElementById('name').value.trim()
     const password = document.getElementById('password').value.trim()
 
-    if(email && password) {
+    if(name && password) {
         const response = await fetch('/api/users/login', {
             method: 'POST', 
-            body: JSON.stringify({ email, password }),
+            body: JSON.stringify({ name, password }),
             headers: { 'Content-Type': 'application/json' },
         })   
         if (response.ok) {
--- public/js/signup.js
+++ public/js/signup.js
@@ -4,12 +4,11 @@ loginBtn.addEventListener('click', async (e) => {
     e.preventDefault();
     const name = document.getElementById('username').value.trim()
     const password = document.getElementById('password').value.trim()
-    const email = document.getElementById('email').value.trim()
 
     if(name && password && email) {
         const response = await fetch('/api/users/signup', {
             method: 'POST', 
-            body: JSON.stringify({ name, email, password }),
+            body: JSON.stringify({ name, password }),
             headers: { 'Content-Type': 'application/json' },
         })   
         if (response.ok) {
